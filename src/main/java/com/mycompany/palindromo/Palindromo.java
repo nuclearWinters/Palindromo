fe78c1c33a78b6e66a20e43550d6e545b1318409
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2019-09-29 17:15:20 -0500

    cambio de nombre del metodo

--- src/main/java/com/mycompany/palindromo/Palindromo.java
+++ src/main/java/com/mycompany/palindromo/Palindromo.java
@@ -10,12 +10,12 @@ package com.mycompany.palindromo;
  * @author Armando
  */
 public class Palindromo {
-    public static boolean es_Palindromo(String s){
+    public static boolean probarPalindromo(String s){
         if(s.length() <= 1){
             return true;
         }else{
             if(s.charAt(0) == s.charAt(s.length() - 1)){
-                return es_Palindromo(s.substring(1,s.length() - 1 ) );
+                return probarPalindromo(s.substring(1,s.length() - 1 ) );
             }else{
                 return false;
             }
@@ -23,8 +23,8 @@ public class Palindromo {
     }
     public static void main(String[] args) {
         String pali = "ABA";
-        System.out.println(pali + ": " + es_Palindromo(pali));
+        System.out.println(pali + ": " + probarPalindromo(pali));
         pali = "ABAB";
-        System.out.println(pali + ": " + es_Palindromo(pali));
+        System.out.println(pali + ": " + probarPalindromo(pali));
     }
 }
